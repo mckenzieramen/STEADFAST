3eaaf6721f8630509e522dc5d529b459ab84765a
Cliff Jandee Medrano <yahhclffjnd@gmail.com>
2026-09-19 19:49:35 +0800

    Add files via upload

--- email-config.js
+++ email-config.js
@@ -2,7 +2,7 @@
 // No Gmail password or OAuth token is stored here.
 window.STEADFAST_EMAIL_CONFIG = {
   provider: 'google-apps-script',
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbwxfvMp2It4N6z3S3dFEue9-KZ6_LZiweZiU15oVOap1mKZBRgEsEIG_dM3dell1utvEA/exec',
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbzit8ibUWLvji0-hM_PEAe9hLdahzRMp6FXTkTkK3LWUHOmv0I_0iddRCP55ypgmFQEGw/exec',
   adminEmail: 'yahhclffjnd@gmail.com',
   senderName: 'STEADFAST by Cliff Jandee Medrano'
 };
